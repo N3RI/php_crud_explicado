47c7ef6254f05fddb5d0d3885f1a807ce9c8bcf8
N3RI <soy@n3ri.com.ar>
2023-09-21 22:51:58 -0300

    creé crud básico y base de datos

---
+++ create.php
@@ -0,0 +1,24 @@
+<?php
+// Establecer una conexión a la base de datos
+$conexion = mysqli_connect("localhost", "usuario", "contraseña", "base_de_datos");
+
+// Comprobar la conexión
+if (mysqli_connect_errno()) {
+    die("La conexión a la base de datos falló: " . mysqli_connect_error());
+}
+
+// Insertar datos en la tabla 'usuarios'
+$nombre = "Neri Tisocco";
+$email = "neri@tisocco.com";
+$consulta = "INSERT INTO usuarios (apellido, nombre, email) VALUES ('$apellido', '$nombre', '$email')";
+$resultado = mysqli_query($conexion, $consulta);
+
+if ($resultado) {
+    echo "Registro creado con éxito";
+} else {
+    echo "Error: " . mysqli_error($conexion);
+}
+
+// Cerrar la conexión a la base de datos
+mysqli_close($conexion);
+?>
